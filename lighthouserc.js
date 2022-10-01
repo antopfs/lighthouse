6aa934d299030f30aefd5d12f9891e0f738236f4
antopfs <antopfs@gmail.com>
2022-10-01 20:15:58 -0300

    comiit9

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,11 +2,16 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './',
-        url: ['http://127.0.0.1:80/lighthouse/index.html']
+        url: ['http://127.0.0.1:80/']
       },
       upload: {
-        target:lhci
-        //target: 'temporary-public-storage',
+        target: 'temporary-public-storage',
+      },
+      assert: {
+        assertions: {
+          'categories:performance': ['warn', {minScore: 1}],
+          'categories:accessibility': ['error', {minScore: 1}]
+        }
       },
     },
   };
