f30366ae3a58760c64eea7ec4ea5787b5b7aa8b9
antopfs <antopfs@gmail.com>
2022-10-01 18:40:25 -0300

    commit 4

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './public',
-        url: ['http://127.0.0.1/lighthouse/']
+        url: ['http://127.0.0.1/lighthouse/build/index.html']
       },
       upload: {
         target: 'temporary-public-storage',
